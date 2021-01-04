8da1f1664fcb865f83a69b22b5dd043b29123505
Hunter Heston <hunter_heston@yahoo.com>
2021-01-04 16:49:51 -0500

    calling it done

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,34 @@ router.get("/", async (req, res, next) => {
     };
 
     for (const link of read.links) {
-      entry.links.push({ description: link.description, link: link.link });
+      // const link_short_name = [...link.link];
+
+      let link_short_name = link.link;
+
+      link_short_name = link_short_name.slice(link_short_name.indexOf(".") + 1);
+      link_short_name = link_short_name.slice(0, link_short_name.indexOf("/"));
+
+      // // get rid of everything before the .
+      // while (link_short_name[0] !== ".") {
+      //   link_short_name.shift();
+      // }
+
+      // // get rid of the .
+      // link_short_name.shift();
+
+      // let i = 0;
+      // while (link_short_name[i] !== "/" && i < link_short_name.length) {
+      //   i++;
+      // }
+
+      // // remove everything after
+      // link_short_name.splice(i);
+
+      entry.links.push({
+        description: link.description,
+        link: link.link,
+        link_short_name: link_short_name,
+      });
     }
 
     view_data.push(entry);
