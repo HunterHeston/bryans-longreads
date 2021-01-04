993e006f22da78b92e4bbf15ef14c4235f91c32d
Hunter Heston <hunter_heston@yahoo.com>
2021-01-04 14:07:52 -0500

    displaying links on a page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  // query the database
+  const long_reads = await prisma.longreadList.findMany({
+    include: {
+      links: true,
+    },
+  });
+
+  // create an object to pass to our view
+  const view_data = [];
+
+  for (const read of long_reads) {
+    const entry = {
+      date: read.date,
+      links: [],
+    };
+
+    for (const link of read.links) {
+      entry.links.push({ description: link.description, link: link.link });
+    }
+
+    view_data.push(entry);
+  }
+
+  res.render("index", { view_data: view_data });
 });
 
 module.exports = router;
